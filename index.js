0e49884249b572d7b20af5ca14aa071756adfdf4
Dharanish P M <dharanishpm@gmail.com>
2023-01-03 11:24:35 +0530

    loginpage

--- index.js
+++ index.js
@@ -7,15 +7,28 @@ function myfun() {
 }
 
 const login = document.querySelector(".login");
-const signup=document.querySelector(".signup");
+const signup = document.querySelector(".signup");
+
 login.addEventListener('click', () => {
     login.classList.remove("ac");
     signup.classList.add("ac");
+        document.querySelector(".email").style.display = "none";
+    document.querySelector(".confirmpass").style.display = "none";
+    
+   
+
 })
 signup.addEventListener('click', () => {
-     signup.classList.remove("ac");
+    signup.classList.remove("ac");
     login.classList.add("ac");
-   
+    document.querySelector(".email").style.display = "block";
+    document.querySelector(".confirmpass").style.display = "block";
+    document.querySelector(".submit").innerHTML="SignUp"
+
+  
+    
+
 })
+
 
 
