2092d5ab03dda474c8822c56e4ca10c803f11042
Dharanish P M <dharanishpm@gmail.com>
2023-01-03 10:15:52 +0530

    update

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ function myfun() {
     password = document.querySelector("#pass");
 }
 
+const login = document.querySelector(".login");
+const signup=document.querySelector(".signup");
+login.addEventListener('click', () => {
+    login.classList.remove("ac");
+    signup.classList.add("ac");
+})
+signup.addEventListener('click', () => {
+     signup.classList.remove("ac");
+    login.classList.add("ac");
+   
+})
+
 
