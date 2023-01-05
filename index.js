1bb383d49cce48e6cd160e523b992b8424767d01
Dharanish P M <dharanishpm@gmail.com>
2023-01-05 14:46:58 +0530

    login page server connect

--- index.js
+++ index.js
@@ -1,37 +1,68 @@
 
 const login = document.querySelector(".login");
 const signup = document.querySelector(".signup");
+var form1 = document.querySelector('.log');
+var form2 = document.querySelector('.sign');
 
+var submit = document.querySelector('.submit');
 login.addEventListener('click', () => {
     login.classList.remove("ac");
     signup.classList.add("ac");
-     document.querySelector(".email").classList.add('disable');
-    document.querySelector(".confirmpass").classList.add('disable');
-    document.querySelector(".submit").innerHTML = "Login";
+    
+     form1.classList.remove("disable");
+    form2.classList.add("disable");
+
+    form1.addEventListener('submit', (e) => {
+    e.preventDefault();//autosubmission suppress
+        const inputs = form1.elements;
+        
+    const data = {
+        "email": inputs["curremail"].value,
+        "password": inputs["password"].value
+       
+    }
+   
+    fetch('http://192.168.236.5:5000/login', {
+    
+        method: 'POST', // or 'PUT'
+        headers: {     
+            'Accept': 'application/json,,text/plain,*/*',
+            'Content-Type': 'application/json'      
+        },
+        body: JSON.stringify(data)
+   
+    }).then((response) => response.json())
+        .then((data) => {
+            if (data['status'] == 'Success') {
+                window.location.href = "/home.html"
+                
+            }
+        })
+        .catch((error) => {
+             console.dir('Error:', error);
+        });
+    
+})
+ 
 })
 signup.addEventListener('click', () => {
     signup.classList.remove("ac");
     login.classList.add("ac");
-  
-    document.querySelector(".email").classList.remove('disable');
-    document.querySelector(".confirmpass").classList.remove('disable');
-    document.querySelector(".submit").innerHTML = "SignUp";
-})
+    form1.classList.add("disable");
+    form2.classList.remove("disable");
 
-var form = document.querySelector('.log');
-var submit=document.querySelector('.submit');
-form.addEventListener('submit', (e) => {
+    form2.addEventListener('submit', (e) => {
     e.preventDefault();//autosubmission suppress
-    const inputs = form.elements;
+    const inputs = form2.elements;
     const data = {
-        "username": inputs["username"].value,
-        "password": inputs["password"].value,
+        "username": inputs["newusername"].value,
+        "password": inputs["newpassword"].value,
         "email":inputs["email"].value
     }
    
-    console.log(data);
+ 
    
-    fetch('http://10.140.16.52:5000/login', {
+    fetch('http://192.168.236.5:5000/signup', {
     
         method: 'POST', // or 'PUT'
         headers: {     
@@ -42,11 +73,31 @@ form.addEventListener('submit', (e) => {
         body: JSON.stringify(data)
    
     }).then((response) => response.json())
-      .then((data) => {
-           console.log('Success:', data); 
+        .then((data) => {
+              console.log( data); 
         })
         .catch((error) => {
              console.dir('Error:', error);
         });
+
+    
     
 })
+   
+
+
+})
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
