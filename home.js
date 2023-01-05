be38b25f61cba82cf25d1e060cb2631d5cc61abd
Dharanish P M <dharanishpm@gmail.com>
2023-01-05 17:08:45 +0530

    post api successful

--- home.js
+++ home.js
@@ -1,9 +1,19 @@
-function like_post() {
-    let like = document.querySelector('#like');
-   
-    like.style.color='red';
-    
-    
-}
+console.log('Bearer '+localStorage.getItem("token"));
 
-document.getElementById('like').addEventListener("click",like_post);
+ fetch('http://192.168.236.5:5000/showpost', {
+    
+        method: 'POST', // or 'PUT'
+     headers: {    
+         'Authorization':'Bearer '+localStorage.getItem("token"),
+            'Accept': 'application/json,,text/plain,*/*',
+            'Content-Type': 'application/json'      
+        },
+        body: ""
+   
+    }).then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+        })
+        .catch((error) => {
+             console.dir('Error:', error);
+        });
