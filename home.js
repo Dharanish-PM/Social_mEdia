ad5af39976747cdb2e34dac6587d9ba959414c82
Dharanish P M <dharanishpm@gmail.com>
2023-01-08 22:04:21 +0530

    posting deleting comment funtinolaties done

--- home.js
+++ home.js
@@ -1,141 +1,293 @@
-
+const url = "http://192.168.128.182:5000";
 //create post
 const postform = document.querySelector("#postform");
 const infile = document.querySelector("#infile");
-var srcdata=""
+var srcdata = "";
 function imageUploaded() {
-    
-    var fileselect = document.querySelector("#infile").files;
-    if (fileselect.length > 0) {
-        fileselect = fileselect[0];
-        var fd = new FileReader();
-        fd.onload = (loadevent) => {
-            srcdata = loadevent.target.result;
-            document.querySelector(".imagefile").src = srcdata;
-            
-            srcdata=srcdata.replace("data:", "")
-                .replace(/^.+,/, "");
-            console.log(srcdata);
-            
-        }
-        fd.readAsDataURL(fileselect);
-    }
+  var fileselect = document.querySelector("#infile").files;
+  if (fileselect.length > 0) {
+    fileselect = fileselect[0];
+    var fd = new FileReader();
+    fd.onload = (loadevent) => {
+      srcdata = loadevent.target.result;
+      document.querySelector(".imagefile").src = srcdata;
 
-    
-    }
+      //srcdata=srcdata.replace("data:", "")
+      //.replace(/^.+,/, "");
+      //console.log(srcdata);
+    };
+    fd.readAsDataURL(fileselect);
+  }
+}
+
+getlatest();
 
 postform.addEventListener("submit", (e) => {
-    e.preventDefault();
-    imageUploaded();
+  e.preventDefault();
+  imageUploaded();
+  var description_of_post = document.querySelector("#Description").value;
 
-    var obj = {
-        "image": srcdata,
-        "msg":"Hy"
-        
-    };
-    console.log(obj);
+  var obj = {
+    image: srcdata,
+    msg: description_of_post,
+  };
+  //console.log(obj);
 
-    fetch('http://10.140.16.100:5000/post', {
-    method: 'POST',
+  fetch(url + "/post", {
+    method: "POST",
     headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem("token"),
-        'Accept': 'application/json,text/plain,*/*',
-        'Content-Type': 'application/json'
+      Authorization: "Bearer " + localStorage.getItem("token"),
+      Accept: "application/json,text/plain,*/*",
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(obj)
-    }).then((res) => res.json())
-        .then((data) => console.log(data))
-        .catch((error) => {
-            console.log(error)
-       })
-   
-})
+    body: JSON.stringify(obj),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data["status"] == "Posted Successfully") {
+        //alert(data['status']);
+        getlatest();
+      }
+    })
 
-//fetch all posts
-fetch('http://10.140.16.100:5000/showpost', {
-    method: 'POST',
-    headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem("token"),
-        'Accept': 'application/json,text/plain,*/*',
-        'Content-Type': 'application/json'
-    },
-    body: ""
-}).then((response) => response.json()).then((data) => {
-    console.log(data);
-    var output = ""
-    data.forEach(post => {
-        //parent node
-        let card = document.createElement('div');
-        card.classList.add('card');
-        //user-section
-        let user = document.createElement('div');
-        user.classList.add('user')
-        let user_img = document.createElement('i');
-        user_img.classList.add('icon');
-        user_img.classList.add('fa-regular');
-        user_img.classList.add('fa-user');
-        let para = document.createElement('p');
-        para.innerHTML = post['postid']
-        user.appendChild(user_img);
-        user.appendChild(para);
-        //post image
-        let post_img = document.createElement('div');
-        post_img.classList.add('post-img');
-        let img = document.createElement('img');
-        img.setAttribute('src', 'images/profilepic.jpg');
-        img.setAttribute('height', '400');
-        img.setAttribute('width', '450');
-        post_img.appendChild(img);
-        //like comment share
-        let options = document.createElement('div');
-        options.classList.add('post-like');
-        let like = document.createElement('i');
-        like.classList.add('like');
-        like.classList.add('fa-solid');
-        like.classList.add('fa-heart');
-        let comment = document.createElement('i');
-        comment.classList.add('comment');
-        comment.classList.add('fa-regular');
-        comment.classList.add('fa-comment');
-        options.appendChild(like);
-        options.appendChild(comment);
-        let desc = document.createElement('div');
-        desc.classList.add('desc');
-        let pp = document.createElement('p');
-        pp.innerHTML = "Title : " + post['title']
-        let bb = document.createElement('p');
-        bb.innerHTML = "Body : " + post['created_time'];
-        desc.appendChild(pp);
-        desc.appendChild(bb);
-        card.appendChild(user);
-        card.appendChild(post_img);
-        card.appendChild(options);
-        card.appendChild(desc);
-        document.querySelector(".posts").appendChild(card);
-        console.log("from post returns");
-        const likes = {
-            'postid': post['postid']
-        }
-        like.addEventListener('click', () => {
-            like.style.color = "red";
-            fetch('http://10.140.16.100:5000/like', {
-                method: 'POST',
-                headers: {
-                    'Authorization': 'Bearer ' + localStorage.getItem("token"),
-                    'Accept': 'application/json,text/plain,*/*',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(likes)
-            }).then((res) => res.json()).then((data) => {
-                console.log(data);
-            }).catch((error) => {
-                console.dir(error)
-            });
-        })
+    .catch((error) => {
+      console.log(error);
     });
-}).catch((error) => {
-    console.dir('Error:', error);
 });
 
+//fetch all posts
+function getlatest() {
+  document.querySelector(".posts").innerHTML = "";
+  fetch(url + "/latestpost", {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("token"),
+      Accept: "application/json,text/plain,*/*",
+      "Content-Type": "application/json",
+    },
+    body: "",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      //console.log("data");
+      console.log(data);
 
+      data.forEach((post) => {
+        //parent node
 
+        let card = document.createElement("div");
+        card.classList.add("card");
+
+        //user-section
+        let user = document.createElement("div");
+        user.classList.add("user");
+        let left = document.createElement("div");
+        left.classList.add("left");
+        let user_img = document.createElement("i");
+        user_img.classList.add("icon");
+        user_img.classList.add("fa-regular");
+        user_img.classList.add("fa-user");
+        let para = document.createElement("p");
+        para.innerHTML = post["body"];
+        left.appendChild(user_img);
+        left.appendChild(para);
+        let trash = document.createElement("i");
+        trash.classList.add("icon");
+        trash.classList.add("trash");
+        trash.classList.add("fa-solid");
+        trash.classList.add("fa-trash");
+        user.appendChild(left);
+        user.appendChild(trash);
+        //post image
+        let img = document.createElement("img");
+        img.setAttribute("src", post["image"]);
+        //like comment share
+        let options = document.createElement("div");
+        options.classList.add("post-like");
+        let like = document.createElement("i");
+        like.classList.add("like");
+        like.classList.add("icon");
+        like.classList.add("fa-solid");
+        like.classList.add("fa-heart");
+        let comment = document.createElement("i");
+        comment.classList.add("comment");
+        comment.classList.add("icon");
+        comment.classList.add("fa-regular");
+        comment.classList.add("fa-comment");
+        options.appendChild(like);
+        options.appendChild(comment);
+        let desc = document.createElement("div");
+        desc.classList.add("desc");
+        let Nu = document.createElement("p");
+        Nu.innerHTML = "Likes : " + post["no_of_likes"];
+        desc.appendChild(Nu);
+
+        // let pp = document.createElement("p");
+        // pp.innerHTML = "Title : " + post["title"];
+        let bb = document.createElement("p");
+        bb.innerHTML = "Description : " + post["msg"];
+        let comments = document.createElement("div");
+        comments.classList.add("comments");
+
+        //post comment
+        let com_desc = document.createElement("div");
+        com_desc.setAttribute("id", "com_desc");
+        comments.appendChild(com_desc);
+
+        let post_co = document.createElement("form");
+        let inp = document.createElement("input");
+        inp.setAttribute("type", "text");
+        inp.setAttribute("placeholder", "Add comment");
+        inp.setAttribute("id", "inputcomment");
+
+        let inp_sub = document.createElement("input");
+        inp_sub.setAttribute("type", "submit");
+        inp_sub.innerHTML = "submit";
+        post_co.appendChild(inp);
+        post_co.appendChild(inp_sub);
+        comments.appendChild(post_co);
+
+        //desc.appendChild(pp);
+        desc.appendChild(bb);
+        card.appendChild(user);
+        card.appendChild(img);
+        card.appendChild(options);
+        card.appendChild(desc);
+        card.appendChild(comments);
+
+        document.querySelector(".posts").appendChild(card);
+
+        //post id of current post
+        const curr_postid = {
+          postid: post["postid"],
+        };
+
+        like.addEventListener("click", () => {
+          likepost();
+        });
+
+        function likepost() {
+          fetch(url + "/like", {
+            method: "POST",
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("token"),
+              Accept: "application/json,text/plain,*/*",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(curr_postid),
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              Nu.innerHTML = "Likes : " + data["no_of_likes"];
+            })
+            .catch((error) => {
+              console.dir(error);
+            });
+        }
+
+        function latestcomment() {
+          com_desc.innerHTML = "";
+
+          fetch(url + "/showcomment", {
+            method: "POST",
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("token"),
+              Accept: "application/json,text/plain,*/*",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(curr_postid),
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              data.forEach((ele) => {
+                let ind_com = document.createElement("div");
+                ind_com.classList.add("individual_comment");
+
+                user_label = document.createElement("p");
+                user_label.innerHTML = "username :";
+                var usercomment = document.createElement("p");
+                usercomment.classList.add("usercomment");
+                usercomment.innerHTML = ele["msg"];
+                ind_com.appendChild(user_label);
+                ind_com.appendChild(usercomment);
+                com_desc.appendChild(ind_com);
+                // comments.insertBefore(user_label,post_co);
+                // comments.insertBefore(usercomment,post_co);
+              });
+            })
+            .catch((error) => {
+              console.dir(error);
+            });
+        }
+        //get comments for each post
+
+        comment.addEventListener("click", () => {
+          latestcomment();
+        });
+
+        //post comment
+        function postcomment(c) {
+          fetch(url + "/comment", {
+            method: "POST",
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("token"),
+              Accept: "application/json,text/plain,*/*",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(c),
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              //console.log(data);
+              if (data["status"] == "Success") {
+                latestcomment();
+              }
+            })
+            .catch((error) => {
+              console.dir(error);
+            });
+        }
+
+        post_co.addEventListener("submit", (e) => {
+          e.preventDefault();
+          var new_comment = inp.value;
+          // console.log(new_comment);
+          var c = {
+            postid: post["postid"],
+            msg: new_comment,
+          };
+          postcomment(c);
+        });
+
+        //delete post
+        function deletepost() {
+          fetch(url + "/deletepost", {
+            method: "POST",
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("token"),
+              Accept: "application/json,text/plain,*/*",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(curr_postid),
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              console.log(data);
+              if (data["status"] == "Success") {
+                getlatest();
+              }
+            })
+            .catch((error) => {
+              console.dir(error);
+            });
+        }
+
+        trash.addEventListener("click", () => {
+          deletepost();
+        });
+      });
+    })
+    .catch((error) => {
+      console.dir("Error:", error);
+    });
+}
--- index.js
+++ index.js
@@ -1,98 +1,83 @@
-
 const login = document.querySelector(".login");
 const signup = document.querySelector(".signup");
-var form1 = document.querySelector('#logform');
-var form2 = document.querySelector('.sign');
-form1.addEventListener('submit', (e) => {
-    e.preventDefault();//autosubmission suppress
-        const inputs = form1.elements;
-        
-    const data = {
-        "email": inputs["curremail"].value,
-        "password": inputs["password"].value
-       
-        }
-   
-    fetch('http://10.140.16.100:5000/login', {
-    
-        method: 'POST', // or 'PUT'
-        headers: {     
-            'Accept': 'application/json,,text/plain,*/*',
-            'Content-Type': 'application/json'      
-        },
-        body: JSON.stringify(data)
-   
-    }).then((response) => response.json())
-        .then((data) => {
-            console.log(data["token"])
-            localStorage.setItem("token", data["token"])
-            if (data['status'] == 'Success') {
-                //add login successful
-                window.location.href = "/home.html"
-            }
-            else {
-                //no user exists
-                document.querySelector(".error").classList.remove("disable");
-                document.querySelector(".error").innerHTML = data['status'];
+var form1 = document.querySelector("#logform");
+var form2 = document.querySelector(".sign");
+form1.addEventListener("submit", (e) => {
+  e.preventDefault(); //autosubmission suppress
+  const inputs = form1.elements;
 
+  const data = {
+    email: inputs["curremail"].value,
+    password: inputs["password"].value,
+  };
 
-            }
-        })
-        .catch((error) => {
-             console.dir('Error:', error);
-        });
+  fetch("http://192.168.128.182:5000/login", {
+    method: "POST", // or 'PUT'
+    headers: {
+      Accept: "application/json,,text/plain,*/*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data["token"]);
+      localStorage.setItem("token", data["token"]);
+      if (data["status"] == "Success") {
+        //add login successful
+        window.location.href = "/home.html";
+      } else {
+        //no user exists
+        document.querySelector(".error").classList.remove("disable");
+        document.querySelector(".error").innerHTML = data["status"];
+      }
+    })
+    .catch((error) => {
+      console.dir("Error:", error);
+    });
+});
+login.addEventListener("click", () => {
+  login.classList.remove("ac");
+  signup.classList.add("ac");
 
-     
-    
-})
-login.addEventListener('click', () => {
-    login.classList.remove("ac");
-    signup.classList.add("ac");
-    
-     form1.classList.remove("disable");
-    form2.classList.add("disable");
-})
-signup.addEventListener('click', () => {
-    signup.classList.remove("ac");
-    login.classList.add("ac");
-    form1.classList.add("disable");
-    form2.classList.remove("disable");
+  form1.classList.remove("disable");
+  form2.classList.add("disable");
+});
+signup.addEventListener("click", () => {
+  signup.classList.remove("ac");
+  login.classList.add("ac");
+  form1.classList.add("disable");
+  form2.classList.remove("disable");
 
-    form2.addEventListener('submit', (e) => {
-    e.preventDefault();//autosubmission suppress
+  form2.addEventListener("submit", (e) => {
+    e.preventDefault(); //autosubmission suppress
     const inputs = form2.elements;
     const data = {
-        "username": inputs["newusername"].value,
-        "password": inputs["newpassword"].value,
-        "email":inputs["email"].value
-    }
-   
-  
- 
-   
-    fetch('http://10.140.16.100:5000/signup', {
-    
-        method: 'POST', // or 'PUT'
-        headers: {     
-            'Accept': 'application/json,,text/plain,*/*',
-            'Content-Type': 'application/json'
-            
-        },
-        body: JSON.stringify(data)
-   
-    }).then((response) => response.json())
-        .then((data) => {
-              console.log( data); 
-        })
-        .catch((error) => {
-             console.dir('Error:', error);
-        }); 
-        
+      username: inputs["newusername"].value,
+      password: inputs["newpassword"].value,
+      email: inputs["email"].value,
+    };
+
+    fetch("http://192.168.128.182:5000/signup", {
+      method: "POST", // or 'PUT'
+      headers: {
+        Accept: "application/json,,text/plain,*/*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.dir("Error:", error);
+      });
+
     alert("Signup is successful Please login");
     window.location.href = "/index.html";
-})  
-})
-
+  });
+});
 
 /*
 function checkpassword() {
@@ -113,19 +98,3 @@ function checkpassword() {
 document.querySelector("#confirmpass").addEventListener('change', () => {
     checkpassword();
 })*/
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
